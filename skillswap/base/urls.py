2dca4d7990cd26e2e21d605bd64e8395ff1a24ee
Nikhil kumar <nikhilraj7654@gmail.com>
2024-05-17 18:41:21 +0530

    view and template for activity, navbar, feed and topic

--- skillswap/base/urls.py
+++ skillswap/base/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns =[
@@ -7,7 +9,9 @@ urlpatterns =[
     path('register/',views.registerPage, name="register"),
     path('',views.home, name="home"),
     path('room/<str:pk>/',views.room, name="room"),
+    path('profile/<str:pk>/',views.userProfile, name="user-profile"),
     path('create-room/',views.createRoom, name="create-room"),
     path('update-room/<str:pk>/',views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/',views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/',views.deleteMessage, name="delete-message"),
 ]
